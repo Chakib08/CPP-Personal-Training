7342c64eb6a3aba0d854c65f7987d54bc107b5fc
Mohamed Lamine Kartobi <chakibdace@gmail.com>
2024-02-15 08:56:04 +0100

    Add set part (Only in the main)

--- CPP_Personal/main.cpp
+++ CPP_Personal/main.cpp
@@ -7,6 +7,8 @@
 #include "Tuple/tuple.h"
 #include "Map/map.h"
 
+#include <set>
+
 int main(int argc, char *argv[])
 {
 
@@ -15,10 +17,34 @@ int main(int argc, char *argv[])
 
 //    Tuple tup;
 
-    Map mp;
-    char c = 'a';
-    std::string str =  "aaba";
-    std::cout << "Freq of the char is " << mp.getCharFreq(str, c) << std::endl;
+//    Map mp;
+//    char c = 'a';
+//    std::string str =  "aaba";
+//    std::cout << "Freq of the char is " << mp.getCharFreq(str, c) << std::endl;
 
+    /* set are contrainers that stores elements without keeping the duplicate */
+    std::set<int> sett = {1, 2 , 3, 1, 2};
+//    for(auto &s : sett) {
+//        std::cout << s << std::endl;
+
+//    }
+
+    /* Example of retreiving the set in a vector (Dynamic array) */
+//    std::vector<int> vc(sett.begin(), sett.end());
+//    for (auto &v : vc) {
+//        std::cout << v << std::endl;
+//    }
+
+    // Creating a vector and copying elements from the set
+    std::vector<int> vec;
+    for (int num : sett) {
+        vec.push_back(num);
+    }
+
+    // Now vec contains the elements of sett
+    for (int num : vec) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
     return 0;
 }
